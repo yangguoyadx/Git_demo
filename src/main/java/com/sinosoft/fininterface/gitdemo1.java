0b8bedaa2729f7d4de04ada5922d25cbca886120
yangguo <yangguosz@sinosoft.com.cn>
2023-09-18 17:30:14 +0800

    hot-fix first commit

--- src/main/java/com/sinosoft/fininterface/gitdemo1.java
+++ src/main/java/com/sinosoft/fininterface/gitdemo1.java
@@ -7,5 +7,6 @@ public class gitdemo1 {
     public static void main(String[] args) {
         System.out.println("Hello git");
         System.out.println("Hello git2");
+        System.out.println("Hello git3");
     }
 }
